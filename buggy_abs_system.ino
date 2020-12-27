889a5ce5519286946a195b944770bf51f4e3149d
Guy Raiz SpearUAV <guy@spearuav.com>
2020-12-27 16:42:56 +0200

    further cleanup + syntaxing

--- buggy_abs_system.ino
+++ buggy_abs_system.ino
@@ -1,4 +1,9 @@
+void wheel_speed_calculation(void);
+
+
 float wheel_speed(long unsigned int, long unsigned int);
+void rightwheelping (void);
+void leftwheelping (void);
 
 //input pins
 const int right_wheel_sensor_pin = 2;
@@ -22,79 +27,39 @@ const int RIM_DIAMETER_INCH = 21;
 const int TYRE_CIRCUMFRENCE_MM = (TYRE_WIDTH_MM / 100 * TYRE_PROFILE_PERCENT * 2 + RIM_DIAMETER_INCH * 25.4) * PI;
 
 
-
+//timers
 long unsigned int right_wheel_timer;
 long unsigned int previous_right_wheel_timer;
 long unsigned int left_wheel_timer;
 long unsigned int previous_left_wheel_timer;
 
 
-#define rightwheelpin 1 // in inches
-#define leftwheelpin 2 // in inches
-#define tirediameter 20 // in inches
-#define pings 1 // number of pings expected for a single revolution
-#define mindelta 3 //minimum speed delta between right and left wheels to initiate compensation
-#define rightsole1 4 //pin for 1st right solenoid
-#define rightsole2 5 //pin for 2nd right solenoid
-#define rightsole3 6 //pin for 3rd right solenoid
-#define leftsole1 7 //pin for 1st left solenoid
-#define leftsole2 8 //pin for 2nd left solenoid
-#define leftsole3 9 //pin for 3rd left solenoid
-#define frontsole1 10 //pin for 1st front solenoid
-#define frontsole2 11 //pin for 2nd front solenoid
-#define frontsole3 12 //pin for 3rd front solenoid
-#define pumppin 13 //pin for pump
-#define difflockpin 14 //pin for diff lock button
-#define rearlockpin 15 //pin for diff lock button
+float right_wheel_speed;
+float left_wheel_speed;
+bool run_left_wheel_speed_function;
+bool run_right_wheel_speed_function;
 
 
-bool rightback[3]; //solenoids for rear right brake
-bool leftback[3];  //solenoids for rear left brake
-bool front[3];   //solenoids for fornt brakes
-// solenoids are open while booleans are LOW
-int pump; // abs pump pwm value
-bool difflock; // store if difflock is on or off
-bool rearlock; // store if rearlock is on or off
 
-const float tirecircumfrence = tirediameter * 0.0254 * PI; // calculates tire circumfrence
-long unsigned int rightwheeltimer; // timer for calculating right wheel speed
-long unsigned int leftwheeltimer; // timer for calculating left wheel speed
-float rightmps; // right wheel speed
-float leftmps; // left wheel speed
-float speeddelta; //speed difference between rear wheels
+//bool rightback[3]; //solenoids for rear right brake
+//bool leftback[3];  //solenoids for rear left brake
+//bool front[3];   //solenoids for fornt brakes
+//// solenoids are open while booleans are LOW
+//int pump; // abs pump pwm value
+//bool difflock; // store if difflock is on or off
+//bool rearlock; // store if rearlock is on or off
+//
+//const float tirecircumfrence = tirediameter * 0.0254 * PI; // calculates tire circumfrence
+//long unsigned int rightwheeltimer; // timer for calculating right wheel speed
+//long unsigned int leftwheeltimer; // timer for calculating left wheel speed
+//float rightmps; // right wheel speed
+//float leftmps; // left wheel speed
+//float speeddelta; //speed difference between rear wheels
 void setup() {
   attachInterrupt(digitalPinToInterrupt(rightwheelpin), rightwheelping, RISING); //right wheel speed interrupt
   attachInterrupt(digitalPinToInterrupt(leftwheelpin), leftwheelping, RISING); //left wheel speed interrupt
 }
 
 void loop() {
-  difflock = digitalRead (difflockpin);
-  rearlock = digitalRead (rearlockpin);
-  if (difflock) {
-    speeddelta = rightmps - leftmps; //calculate speed difference between right and left wheels. negative values are left faster than right
-    // negative values are left faster than right
-    if (speeddelta > mindelta) { // enable braking only on right wheel
-      brakeright();
-    }
-    if (speeddelta < -mindelta) { // enable braking only on left wheel
-      brakeleft();
-    }
-    if (speeddelta > -mindelta && speeddelta < mindelta) {
-      nobrake();
-    }
-    pumpcalc();
-    control();
-  }
-
-}
-
-
-void rightwheelping () {
-  previous_right_wheel_timer = right_wheel_timer;
-  right_wheel_timer = millis();
-}
-
-void leftwheelping () {
-  previous_left_wheel_timer = right_wheel_timer;
-  left_wheel_timer = millis();
+  wheel_speed_calculation;
 }
--- wheel_speed.ino
+++ wheel_speed.ino
@@ -1,5 +1,29 @@
-float wheel_speed(long unsigned int x, long unsigned int y){
-int delta = y - x;
-float RPS = 1000 / delta;
-return RPS * TYRE_CIRCUMFRENCE_MM / 1000;
+void wheel_speed_calculation() {
+  if (run_right_wheel_speed_function) {
+    right_wheel_speed = wheel_speed (right_wheel_timer, previous_right_wheel_timer);
+    run_right_wheel_speed_function = false;
+  }
+  if (run_left_wheel_speed_function) {
+    left_wheel_speed = wheel_speed (left_wheel_timer, previous_left_wheel_timer);
+    run_left_wheel_speed_function = false;
+  }
+}
+
+
+float wheel_speed(long unsigned int x, long unsigned int y) {
+  int delta = x - y;
+  float RPS = 1000 / delta;
+  return RPS * TYRE_CIRCUMFRENCE_MM / 1000;
+}
+
+void rightwheelping () {
+  previous_right_wheel_timer = right_wheel_timer;
+  right_wheel_timer = millis();
+  run_right_wheel_speed_function = true;
+}
+
+void leftwheelping () {
+  previous_left_wheel_timer = right_wheel_timer;
+  left_wheel_timer = millis();
+  run_left_wheel_speed_function = true;
 }
